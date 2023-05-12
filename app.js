82142f2dca66fb85e49af7a54d16df62ae77fc2f
ELJRAOUYOMAR <eljraouy.omar@ofppt-edu.ma>
2023-05-12 17:00:55 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const net = require('net');
+
+const server = net.createServer((socket) => {
+    let isFirstMessage = true;
+
+    socket.on('data', (data) => {
+        if (isFirstMessage) {
+            
+            const imei = Buffer.from(data.slice(2)).toString('ascii');  
+            console.log(`IMEI: ${imei}`);
+            isFirstMessage = false;
+        } else {
+            
+            const dataStr = data.toString('hex');
+            console.log(`Received data: ${dataStr}`);
+
+           
+        }
+    });
+
+    socket.on('end', () => {
+        console.log('Connection ended');
+    });
+
+    socket.on('error', (err) => {
+        console.error('Socket error:', err);
+    });
+});
+const adresse="35.223.217.106";
+const port = 9000; 
+server.listen(port,adresse, () => {
+    console.log(`Server listening on port ${port}`);
+});
